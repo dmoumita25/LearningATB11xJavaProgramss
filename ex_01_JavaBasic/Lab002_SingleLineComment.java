26991603631442890d4a6218d66425502892a429
moumitadasgupta <dmoumita2006@gmail.com>
2025-03-10 22:52:18 +0530

    10th March...Pushing Single line comment

---
+++ ex_01_JavaBasic/Lab002_SingleLineComment.java
@@ -0,0 +1,9 @@
+package ex_01_JavaBasic;
+
+public class Lab002_SingleLineComment {
+    public static void main(String[] args) {
+        //Single line comment
+        System.out.println("Hello World!");
+        System.out.println("Hello Universe!");
+    }
+}
