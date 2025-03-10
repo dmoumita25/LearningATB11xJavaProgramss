41faf6971f6bcd23df4ce4419af69e5cb387e24a
moumitadasgupta <dmoumita2006@gmail.com>
2025-03-10 23:07:36 +0530

    10th March...Arithmatic exception

---
+++ ex_01_JavaBasic/Lab007_MathException.java
@@ -0,0 +1,7 @@
+package ex_01_JavaBasic;
+
+public class Lab007_MathException {
+    public static void main(String[] args) {
+        System.out.println(10/0);//compiler will throw as arithmatic exception
+    }
+}
