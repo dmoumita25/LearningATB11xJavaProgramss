48fe8d1430b6cb9cc8e4d6036d95d6efaa1ba3b6
moumitadasgupta <dmoumita2006@gmail.com>
2025-03-10 23:14:22 +0530

    10th March...Used $

--- ex_01_JavaBasic/Lab008_LoginPage.java
+++
@@ -1,8 +0,0 @@
-package ex_01_JavaBasic;
-
-public class Lab008_LoginPage {
-    // name convention - first letter capital and other letter capital also
-    //
-
-    }
-}
---
+++ ex_01_JavaBasic/Lab009_This$Moumita.java
@@ -0,0 +1,7 @@
+package ex_01_JavaBasic;
+
+public class Lab009_This$Moumita {
+    public static void main(String[] args) {
+        System.out.println("This is Moumita");
+    }
+}
