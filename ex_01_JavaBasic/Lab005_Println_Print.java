6f39881539df31f585b15f8828ea167466eecb19
moumitadasgupta <dmoumita2006@gmail.com>
2025-03-10 23:02:36 +0530

    10th March...Println vs print

---
+++ ex_01_JavaBasic/Lab005_Println_Print.java
@@ -0,0 +1,11 @@
+package ex_01_JavaBasic;
+
+public class Lab005_Println_Print {
+    public static void main(String[] args) {
+//        System.out.println("Hello Moumita");
+//        System.out.println("Hello Sam");
+
+        System.out.print("Hello Moumita");
+        System.out.print("Hello sam");
+    }
+}
