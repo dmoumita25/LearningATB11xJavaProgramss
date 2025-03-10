85f36e76d5eb60e798adc7155af9bbc2a6314a2c
moumitadasgupta <dmoumita2006@gmail.com>
2025-03-10 23:22:13 +0530

    10th March...Multi main method

---
+++ ex_01_JavaBasic/Lab011_Multi_main_method.java
@@ -0,0 +1,14 @@
+package ex_01_JavaBasic;
+
+public class Lab011_Multi_main_method {
+    public static void main(int args) {
+
+    }
+    public static void main(String args) {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+
+}
