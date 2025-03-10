60d2f524782511e7277b7c09b5cf0b5fab5540a9
moumitadasgupta <dmoumita2006@gmail.com>
2025-03-10 23:15:52 +0530

    10th March...Very long name as identifier

---
+++ ex_01_JavaBasic/Lab010_Very_Long_Name_Written_As_Identifier.java
@@ -0,0 +1,7 @@
+package ex_01_JavaBasic;
+
+public class Lab010_Very_Long_Name_Written_As_Identifier {
+    public static void main(String[] args) {
+        System.out.println("This is moumita");
+    }
+}
