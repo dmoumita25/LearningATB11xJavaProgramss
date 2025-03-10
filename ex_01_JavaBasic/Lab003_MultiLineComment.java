3e268a9cfee82d472b72c4c7c5f6ad38e5b4ce6e
moumitadasgupta <dmoumita2006@gmail.com>
2025-03-10 22:55:14 +0530

    10th March...Pushing multiline comment

---
+++ ex_01_JavaBasic/Lab003_MultiLineComment.java
@@ -0,0 +1,13 @@
+package ex_01_JavaBasic;
+
+public class Lab003_MultiLineComment {
+    public static void main(String[] args) {
+        /*
+        Course name: Automation
+        Timing:7a.m-8a.m
+        Teacher:Sir
+         */
+        System.out.println("Here is the comment");
+
+    }
+}
