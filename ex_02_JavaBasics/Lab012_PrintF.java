a8eca5139e029cda2d86efb488088a156e1aca7a
moumitadasgupta <dmoumita2006@gmail.com>
2025-03-11 22:34:39 +0530

    11th March ...Table of 9

---
+++ ex_02_JavaBasics/Lab012_PrintF.java
@@ -0,0 +1,9 @@
+package ex_02_JavaBasics;
+
+public class Lab012_PrintF {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.printf("You variable is %s",a);
+
+    }
+}
