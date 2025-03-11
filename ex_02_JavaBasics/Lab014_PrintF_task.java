36a0c1997ec399c2f592643460e39c05731b1734
moumitadasgupta <dmoumita2006@gmail.com>
2025-03-11 22:34:39 +0530

    10th March...Multi main method

---
+++ ex_02_JavaBasics/Lab014_PrintF_task.java
@@ -0,0 +1,33 @@
+package ex_02_JavaBasics;
+
+public class Lab014_PrintF_task {
+    public static void main(String[] args) {
+        int table = 9;
+        System.out.printf("table of ::: %d",table);
+        System.out.println();
+        System.out.printf("9 x 1= %d",table);
+        System.out.println();
+        System.out.printf("9 x 2=%d",table*2);
+        System.out.println();
+        System.out.printf("9 x 3=%d",table*3);
+        System.out.println();
+        System.out.printf("9 x 4=%d",table*4);
+        System.out.println();
+        System.out.printf("9 x 5=%d",table*5);
+        System.out.println();
+        System.out.printf("9 x 6=%d",table*6);
+        System.out.println();
+        System.out.printf("9 x 7=%d",table*7);
+        System.out.println();
+        System.out.printf("9 x 8=%d",table*8);
+        System.out.println();
+        System.out.printf("9 x 9=%d",table*9);
+        System.out.println();
+        System.out.printf("9 x 10=%d",table*10);
+        System.out.println();
+
+
+
+
+    }
+}
