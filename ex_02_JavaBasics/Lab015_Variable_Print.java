4998921aa1b8b6752f9f2b25ff72434efd8111bb
moumitadasgupta <dmoumita2006@gmail.com>
2025-03-11 22:58:45 +0530

    11th March .Pushing the code

---
+++ ex_02_JavaBasics/Lab015_Variable_Print.java
@@ -0,0 +1,18 @@
+package ex_02_JavaBasics;
+
+public class Lab015_Variable_Print {
+    public static void main(String[] args) {
+        int number = 10;
+        int num = 1;
+        System.out.printf("10 + 2 + 1=%d",number+num+2);
+        System.out.println();
+        System.out.printf("10 + 3 + 1=%d",number+num+3);
+        System.out.println();
+        System.out.printf("10 + 4 + 1 =%d",number+num+4);
+        System.out.println();
+        System.out.printf("10 + 5 + 1=%d",number+num+5);
+        System.out.println();
+
+
+    }
+}
