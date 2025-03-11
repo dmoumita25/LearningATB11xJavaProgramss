a312cdf598be80579d5638cf7fa3e17cb531c0a6
Moumita Dasgupta <dmoumita2006@gmail.com>
2025-03-11 22:45:08 +0530

    Delete ex_02_JavaBasics directory

--- ex_02_JavaBasics/Lab013_PrintF1.java
+++
@@ -1,9 +0,0 @@
-package ex_02_JavaBasics;
-
-public class Lab013_PrintF1 {
-    public static void main(String[] args) {
-        int a = 10;
-        int b=20;
-        System.out.printf("your variable is %d %d",b, a);
-    }
-}
